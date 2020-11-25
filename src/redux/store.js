57516e06041cbbb7f5134196c0f817f2b9e5c760
Lera <vaysha@bk.ru>
2020-11-26 02:59:35 +0300

    Add products and reviews reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+//import { composeWithDevTools } from 'redux-devtools-extention'
+import logger from './middleware/logger';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
